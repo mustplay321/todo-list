273ef9140482c1e23ae969df638e5d397d6e5c3f
Kaden <140219475+mustplay321@users.noreply.github.com>
2024-09-08 12:18:14 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+/********************************
+ * Simple To-do list application
+ * 
+ * Kaden Christie
+ * Created: 9/8/24
+ ********************************/
+
+import java.util.Scanner;
+
+public static void main(String[] args){
+    
+}
