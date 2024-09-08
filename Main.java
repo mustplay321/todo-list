6987ad5e825197ec68144868894734d606566c24
Kaden <140219475+mustplay321@users.noreply.github.com>
2024-09-08 18:41:53 -0500

    Update Main.java
    - Added base navigation functionality
    - Added Main class (previously only Task)
    - Added debug task "Math Homework"
    - Added list for storing tasks

--- Main.java
+++ Main.java
@@ -3,10 +3,65 @@
  * 
  * Kaden Christie
  * Created: 9/8/24
+ * Updated: 9/8/24
  ********************************/
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public static void main(String[] args){
-    
+/*
+ * 
+ */
+class Task {
+    String taskName;
+    String taskDescription;
+    String taskDueDate;
+
+    public static void Navigation() {
+        //Print navigation options to the console
+        System.out.println("Hello! Where would you like to start today? " 
+        + "\n1: View all current tasks " 
+        + "\n2: Add a task to the list "
+        + "\n3: Edit a task "
+        + "\n4: Remove a task");
+
+        //Store and respond to user input
+        Scanner userNavigation = new Scanner(System.in);
+        String userNav = userNavigation.nextLine();
+        int convertedUserNav = Integer.parseInt(userNav);
+        userNavigation.close();
+
+        switch(convertedUserNav) {
+            case 1:
+            //System.out.println(currentTasks);
+            break;
+            case 2:
+            //add task to list
+            break;
+            case 3:
+            //edit task
+            break;
+            case 4:
+            //remove task
+        }
+    }
+
+    public Task(String taskName, String taskDescription, String taskDueDate) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskDueDate = taskDueDate;
+    }
+
+
+}
+
+public class Main {
+    static List<Task> currentTasks = new ArrayList<>();
+    public static void main(String[] args){
+        Task.Navigation();
+        Task newTask = new Task("Math Homework", "Do Math Homework", "Tuesday");
+        currentTasks.add(newTask);
+        System.out.println(currentTasks);
+    }
 }
