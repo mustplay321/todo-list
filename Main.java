1fc6d0176c97dd1f99a1b03ab60db68713c39727
Kaden <140219475+mustplay321@users.noreply.github.com>
2024-09-10 00:04:58 -0500

    Updated Main.java
    - Added print functions for speed's sake
    - Went crazy trying to print list/add to list
    - Changed task variables to instance rather than static
    - Probably more but its late

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  * 
  * Kaden Christie
  * Created: 9/8/24
- * Updated: 9/8/24
+ * Updated: 9/9/24
  ********************************/
 
 import java.util.Scanner;
@@ -13,14 +13,28 @@ import java.util.List;
 /*
  * 
  */
+
 class Task {
     String taskName;
     String taskDescription;
     String taskDueDate;
 
+    static List<Task> currentTasks = new ArrayList<>();
+
+    public Task(String taskName, String taskDescription, String taskDueDate) {
+
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskDueDate = taskDueDate;
+
+        currentTasks.add(this);
+
+    }
+
     public static void Navigation() {
+
         //Print navigation options to the console
-        System.out.println("Hello! Where would you like to start today? " 
+        println("Hello! Where would you like to start today? " 
         + "\n1: View all current tasks " 
         + "\n2: Add a task to the list "
         + "\n3: Edit a task "
@@ -28,13 +42,14 @@ class Task {
 
         //Store and respond to user input
         Scanner userNavigation = new Scanner(System.in);
+        Scanner newTask = new Scanner(System.in);
         String userNav = userNavigation.nextLine();
         int convertedUserNav = Integer.parseInt(userNav);
-        userNavigation.close();
 
         switch(convertedUserNav) {
             case 1:
-            //System.out.println(currentTasks);
+            //Print current tasks list
+            Task.Navigation();
             break;
             case 2:
             //add task to list
@@ -44,24 +59,41 @@ class Task {
             break;
             case 4:
             //remove task
+
         }
     }
 
-    public Task(String taskName, String taskDescription, String taskDueDate) {
-        this.taskName = taskName;
-        this.taskDescription = taskDescription;
-        this.taskDueDate = taskDueDate;
+
+
+    //Print methods
+    public static void println(String string) {
+        System.out.println(string);
     }
 
-
+    public static void print(String string) {
+        System.out.print(string);
+    }
 }
 
 public class Main {
-    static List<Task> currentTasks = new ArrayList<>();
     public static void main(String[] args){
+
+        //Task testTask = new Task("Math Homework", "Do Math Homework", "Tuesday");
+        //Task testTask1 = new Task("Math Homework", "Do Math Homework", "Tuesday");
+        //Task testTask2 = new Task("Math Homework", "Do Math Homework", "Tuesday");
+        //Task testTask3 = new Task("Math Homework", "Do Math Homework", "Tuesday");
         Task.Navigation();
-        Task newTask = new Task("Math Homework", "Do Math Homework", "Tuesday");
-        currentTasks.add(newTask);
-        System.out.println(currentTasks);
+        
+    }
+
+
+
+    //Print methods
+    public static void println(String string) {
+        System.out.println(string);
+    }
+
+    public static void print(String string) {
+        System.out.print(string);
     }
 }
