764954fc2f77d4a237f387f25d498fce6380ec53
Kaden <140219475+mustplay321@users.noreply.github.com>
2024-09-10 10:27:39 -0500

    Update Main.java
    - Added ability to return to Navigation/end program
    - Added printing task list feature

--- Main.java
+++ Main.java
@@ -18,6 +18,8 @@ class Task {
     String taskName;
     String taskDescription;
     String taskDueDate;
+    
+    static Scanner userNavigation = new Scanner(System.in);
 
     static List<Task> currentTasks = new ArrayList<>();
 
@@ -41,18 +43,28 @@ class Task {
         + "\n4: Remove a task");
 
         //Store and respond to user input
+        Scanner newTaskInput = new Scanner(System.in);
         Scanner userNavigation = new Scanner(System.in);
-        Scanner newTask = new Scanner(System.in);
         String userNav = userNavigation.nextLine();
         int convertedUserNav = Integer.parseInt(userNav);
 
         switch(convertedUserNav) {
             case 1:
             //Print current tasks list
+            if (currentTasks.size() > 0) {
+                for (int i = 0; i < currentTasks.size(); i++) {
+                    println("Current Tasks: " + currentTasks.get(i).taskName);
+                }
+            }
+            else {
+                println("There are currently no tasks on your list. Add some!");
+            }
             Task.Navigation();
             break;
             case 2:
             //add task to list
+            
+            Task.Cont();
             break;
             case 3:
             //edit task
@@ -63,6 +75,19 @@ class Task {
         }
     }
 
+    public static void Cont() {
+        println("Return to Navigation? y/n: ");
+        String cont = userNavigation.nextLine();
+        if (cont.equals("y")) {
+            Task.Navigation();
+        }
+        else if (cont.equals("n")) {
+            System.exit(0);
+        }
+        else {
+            println("Invalid Entry");
+        }
+    }
 
 
     //Print methods
@@ -78,7 +103,6 @@ class Task {
 public class Main {
     public static void main(String[] args){
 
-        //Task testTask = new Task("Math Homework", "Do Math Homework", "Tuesday");
         //Task testTask1 = new Task("Math Homework", "Do Math Homework", "Tuesday");
         //Task testTask2 = new Task("Math Homework", "Do Math Homework", "Tuesday");
         //Task testTask3 = new Task("Math Homework", "Do Math Homework", "Tuesday");
